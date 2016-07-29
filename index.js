bc296c5f720f8e8d54f5714cdddf1d451a6858e5
Ken Egbuna <kegbuna@gmail.com>
2016-07-29 19:00:54 -0400

    proof of concept making api calls against kole links endpoint

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-var request = require('request');
+const apiConfig = require('./config/api.json');
+const logger = require('log4js').getLogger('Kole Runner');
+logger.setLevel('DEBUG');
 
+const request = require('request');
+const _ = require('lodash');
+
+const config = {
+  user: process.env.kole_user,
+  apiKey: process.env.kole_api_key,
+};
+
+const koleUrl = _.template(apiConfig.entry)(config);
+
+logger.debug(`Base URL is ${koleUrl}`);
+
+request(koleUrl, null, (err, response, body) => {
+  logger.debug(`Response returned: ${body}`);
+});
